f52811520a2fd68d8f2f22c4a0cf2c177aa7ecf1
julianmi <julianmi@users.noreply.github.com>
2016-02-26 18:45:39 +0100

    Fixes for CI

--- tools/windows_deploy.bat
+++ tools/windows_deploy.bat
@@ -1,2 +1,2 @@
-%QTDIR%\bin\windeployqt %APPVEYOR_BUILD_FOLDER%\effort-log.exe
-move %APPVEYOR_BUILD_FOLDER%\effort-log.exe %APPVEYOR_BUILD_FOLDER%\effort-log_v%APPVEYOR_BUILD_VERSION%.exe
+%QTDIR%\bin\windeployqt %APPVEYOR_BUILD_FOLDER%\build\release\effort-log.exe --dir dist
+7z a %APPVEYOR_BUILD_FOLDER%\effort-log_v%APPVEYOR_BUILD_VERSION%.zip %APPVEYOR_BUILD_FOLDER%\build\release\dist\*
