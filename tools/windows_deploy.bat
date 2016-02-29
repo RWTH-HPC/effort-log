df3d1aca074e3618842dd7a06a35e7321301ab05
julianmi <julianmi@users.noreply.github.com>
2016-02-29 19:17:24 +0100

    Fixes for automatic deployment

--- tools/windows_deploy.bat
+++ tools/windows_deploy.bat
@@ -1,2 +1,4 @@
-%QTDIR%\bin\windeployqt %APPVEYOR_BUILD_FOLDER%\build\release\effort-log.exe --dir dist
+mkdir %APPVEYOR_BUILD_FOLDER%\build\release\dist
+move %APPVEYOR_BUILD_FOLDER%\build\release\effort-log.exe %APPVEYOR_BUILD_FOLDER%\build\release\dist\effort-log.exe
+%QTDIR%\bin\windeployqt %APPVEYOR_BUILD_FOLDER%\build\release\dist\effort-log.exe
 7z a %APPVEYOR_BUILD_FOLDER%\effort-log_v%APPVEYOR_BUILD_VERSION%.zip %APPVEYOR_BUILD_FOLDER%\build\release\dist\*
