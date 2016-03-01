de54e2da7f6b314f47bd7b341f59fdbe0a44c178
julianmi <julianmi@users.noreply.github.com>
2016-03-01 19:23:08 +0100

    Deployment on tags

--- tools/windows_deploy_encrypted.bat
+++ tools/windows_deploy_encrypted.bat
@@ -3,4 +3,4 @@ mkdir %APPVEYOR_BUILD_FOLDER%\build\release\dist
 copy C:\OpenSSL-Win32\libeay32.dll %APPVEYOR_BUILD_FOLDER%\build\release\dist\libeay32.dll
 copy C:\OpenSSL-Win32\libssl32.dll %APPVEYOR_BUILD_FOLDER%\build\release\dist\libssl32.dll
 copy C:\OpenSSL-Win32\ssleay32.dll %APPVEYOR_BUILD_FOLDER%\build\release\dist\ssleay32.dll
-7z a %APPVEYOR_BUILD_FOLDER%\effort-log_v%APPVEYOR_BUILD_VERSION%_win32_encrypted.zip %APPVEYOR_BUILD_FOLDER%\build\release\dist\*
+7z a %APPVEYOR_BUILD_FOLDER%\effort-log_v%APPVEYOR_REPO_TAG_NAME%_win32_encrypted.zip %APPVEYOR_BUILD_FOLDER%\build\release\dist\*
