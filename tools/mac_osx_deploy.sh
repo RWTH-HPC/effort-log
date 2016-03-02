6f173e3bd4a1c7c563aede431e61fafa339adfab
julianmi <julianmi@users.noreply.github.com>
2016-03-02 12:45:09 +0100

    Fixes for deployment with Travis

--- tools/mac_osx_deploy.sh
+++ tools/mac_osx_deploy.sh
@@ -5,11 +5,9 @@ if [ "$#" -eq 0 ]; then
 else
   APP="EffortLog"
   if [ "$#" -eq 1 ]; then
-    /usr/local/opt/qt5/bin/macdeployqt ${APP}.app -always-overwrite -verbose=2 -qmldir=qml -dmg
-    mv ${APP}.dmg ${APP}_v$1.dmg
+    /usr/local/opt/qt5/bin/macdeployqt ${APP}.app -always-overwrite -verbose=2 -qmldir=qml -dmg && mv ${APP}.dmg ${APP}_$1.dmg
   elif [ $2 = "CRYPT" ]; then
-    /usr/local/opt/qt5/bin/macdeployqt ${APP}.app -always-overwrite -verbose=2 -qmldir=qml -dmg
-    mv ${APP}.dmg ${APP}_v$1_encrypted.dmg
+    /usr/local/opt/qt5/bin/macdeployqt ${APP}.app -always-overwrite -verbose=2 -qmldir=qml -dmg && mv ${APP}.dmg ${APP}_$1_encrypted.dmg
   else
     echo "Unknown parameter < $2 >"
   fi
