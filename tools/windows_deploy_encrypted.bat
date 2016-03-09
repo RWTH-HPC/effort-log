4d9a2f6cbcb2f38a73e6ec2ab77271f0fc95703f
julianmi <julianmi@users.noreply.github.com>
2016-03-09 16:22:29 +0100

    Fixes for missing executable on builds with encryption

--- tools/windows_deploy_encrypted.bat
+++ tools/windows_deploy_encrypted.bat
@@ -1,5 +1,6 @@
 mkdir %APPVEYOR_BUILD_FOLDER%\build\release\dist
-%QTDIR%\bin\windeployqt %APPVEYOR_BUILD_FOLDER%\build\release\effort-log.exe --dir %APPVEYOR_BUILD_FOLDER%\build\release\dist
+move %APPVEYOR_BUILD_FOLDER%\build\release\effort-log.exe %APPVEYOR_BUILD_FOLDER%\build\release\dist\effort-log.exe
+%QTDIR%\bin\windeployqt %APPVEYOR_BUILD_FOLDER%\build\release\dist\effort-log.exe
 copy C:\OpenSSL-Win32\libeay32.dll %APPVEYOR_BUILD_FOLDER%\build\release\dist\libeay32.dll
 copy C:\OpenSSL-Win32\libssl32.dll %APPVEYOR_BUILD_FOLDER%\build\release\dist\libssl32.dll
 copy C:\OpenSSL-Win32\ssleay32.dll %APPVEYOR_BUILD_FOLDER%\build\release\dist\ssleay32.dll
