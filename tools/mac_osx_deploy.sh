4aeb05d4ff3282b0a90687296e9685e1e5cac64e
julianmi <julianmi@users.noreply.github.com>
2016-04-29 11:25:11 +0200

    Small CI fixes

--- tools/mac_osx_deploy.sh
+++ tools/mac_osx_deploy.sh
@@ -5,7 +5,7 @@ if [ "$#" -eq 0 ]; then
 else
   if [ "$#" -eq 1 ]; then
     /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=2 -dmg && mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1.dmg
-  elif [ $2 = "CRYPT" ]; then
+  elif [ $2 = "crypt" ]; then
     /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=2 -dmg && mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_encrypted.dmg
   else
     echo "Unknown parameter < $2 >"
