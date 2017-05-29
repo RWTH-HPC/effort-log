f3e70e08fc77a2d046c5f5c2764a472b22755978
julianmi <julianmi@users.noreply.github.com>
2017-05-29 11:21:02 +0200

    Fixes a CI issue with ruby

--- tools/mac_osx_deploy.sh
+++ tools/mac_osx_deploy.sh
@@ -4,9 +4,15 @@ if [ "$#" -eq 0 ]; then
   echo "Version numbering missing"
 else
   if [ "$#" -eq 1 ]; then
-    /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=3 -appstore-compliant -dmg && mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_osx.dmg
+    /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=3 -appstore-compliant -dmg
+    mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_osx.dmg
+    otool -L ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app/Contents/MacOS/EffortLog
   elif [ $2 = "crypt" ]; then
-    /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=3 -appstore-compliant -dmg && mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_osx_encrypted.dmg
+    install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib @executable_path/../Frameworks/libssl.1.0.0.dylib
+    install_name_tool -change /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks/libcrypto.1.0.0.dylib
+    /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=3 -appstore-compliant -dmg
+    mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_osx_encrypted.dmg
+    otool -L ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app/Contents/MacOS/EffortLog
   else
     echo "Unknown parameter < $2 >"
   fi
