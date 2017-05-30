d775456d259e82e662758264bf000a47865ecd4c
julianmi <julianmi@users.noreply.github.com>
2017-05-30 15:24:02 +0200

    Fixes ruby bug on Travis

--- tools/mac_osx_deploy.sh
+++ tools/mac_osx_deploy.sh
@@ -8,8 +8,10 @@ else
     mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_osx.dmg
     otool -L ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app/Contents/MacOS/EffortLog
   elif [ $2 = "crypt" ]; then
-    install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib @executable_path/../Frameworks/libssl.1.0.0.dylib
-    install_name_tool -change /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks/libcrypto.1.0.0.dylib
+    install_name_tool -id @executable_path/../Frameworks/libssl.1.0.0.dylib EffortLog.app/Contents/Frameworks/libssl.1.0.0.dylib
+    install_name_tool -id @executable_path/../Frameworks/libcrypto.1.0.0.dylib EffortLog.app/Contents/Frameworks/libcrypto.1.0.0.dylib
+    install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib @executable_path/../Frameworks/libssl.1.0.0.dylib EffortLog.app/Contents/MacOS/EffortLog
+    install_name_tool -change /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks/libcrypto.1.0.0.dylib EffortLog.app/Contents/MacOS/EffortLog
     /usr/local/opt/qt5/bin/macdeployqt ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app -always-overwrite -verbose=3 -appstore-compliant -dmg
     mv ${TRAVIS_BUILD_DIR}/build/release/EffortLog.dmg ${TRAVIS_BUILD_DIR}/build/release/EffortLog_$1_osx_encrypted.dmg
     otool -L ${TRAVIS_BUILD_DIR}/build/release/EffortLog.app/Contents/MacOS/EffortLog
