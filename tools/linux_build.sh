4b9790f47b1327188a9a98364401034c619f5a27
julianmi <julianmi@users.noreply.github.com>
2019-04-16 10:48:15 +0200

    Minor fixes to CI

--- tools/linux_build.sh
+++ tools/linux_build.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
-set -e
 
-QMAKE=/usr/local/Cellar/qt/5.12.2/bin/qmake
+echo
+echo "Setup build environment"
+case "$1" in
+    *"ubuntu"*)
+        apt -y update && apt -y install git build-essential qt5-default libssl-dev
+        ;;
+    *"debian"*)
+        apt -y update && apt -y install git build-essential qt5-default libssl-dev
+        ;;
+    *"fedora"*)
+        yum -y update && yum -y install git make qt5-devel openssl-devel
+        ;;
+    *"opensuse"*)
+        zypper -n update && zypper -n install git libQt5Core-devel libQt5Widgets-devel libopenssl-devel
+        ;;
+    *"archlinux"*)
+        pacman -Syu --noconfirm && pacman -S --noconfirm git gcc make qt openssl
+        ;;
+    *)
+        echo "Unknown OS"
+esac
+
 if [ -z "$QMAKE" ]; then
     QMAKE=$(command -v qmake 2>&1)
     if [ -z "$QMAKE" ]; then
@@ -13,12 +33,12 @@ if [ -z "$QMAKE" ]; then
     fi
 fi
 
-echo
-echo "Build environment"
 $QMAKE --version
 if [ -n "$CPP" ]; then
     $CPP --version
 fi
+
+cd /effort-log || exit
 
 echo
 echo "Building debug version"
